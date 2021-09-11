b3c95a619d9e6b93c5a32977e46d7cf2e8700cec
mirwisek <mirwise001@gmail.com>
2021-09-11 20:20:34 +0500

    Updated sql file

--- SmartBusApi/smart_bus.sql
+++ SmartBusApi/smart_bus.sql
@@ -61,6 +61,22 @@ ALTER TABLE `registration`
   ADD PRIMARY KEY (`email`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `buses` (
+  `email` varchar(30) NOT NULL,
+  `isonline` tinyint(1) NOT NULL DEFAULT 0,
+  `currentloc` varchar(100) DEFAULT NULL,
+  `lastloc` varchar(100) DEFAULT NULL,
+  `busno` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `buses` (`email`, `isonline`, `currentloc`, `lastloc`, `busno`) VALUES
+('1@gmail.com', 0, NULL, '30.1774617,67.009527', 'ACB-3121'),
+('2@gmail.com', 1, '30.1894945,66.9980787', '30.18971,66.9982123', 'ACB-3121'),
+('3@gmail.com', 0, NULL, '30.2005474,66.9963533', 'BCD-1231'),
+('4@gmail.com', 0, NULL, '30.194871915854772,67.01767067546771', 'EFG-4321'),
+('5@gmail.com', 0, NULL, '30.179033685315133,67.0145714444933', 'HIJ-9999');
+
+ALTER TABLE `buses`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
